f7010d9a4bb4fb8a46a2ae0cfa8ad337c228e557
aamayorga <amayorga95@outlook.com>
2020-11-16 11:48:23 -0800

    Added Constants file

---
+++ FeedStoreChallenge/Helper/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  FeedStoreChallenge
+//
+//  Created by Antonio Mayorga on 11/16/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct Constant {
+    static let CORE_DATA_FEED_STORE_NAME = "CDFeedStore"
+}
